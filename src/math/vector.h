6e9e0b55789cc21d6cfa687ebd4c4da35cab7b09
Ats <isfendiyar510@gmail.com>
2023-03-22 21:13:11 +0300

    Created vector files

---
+++ src/math/vector.h
@@ -0,0 +1,24 @@
+#ifndef FEF_VECTOR_H
+#define FEF_VECTOR_H
+
+typedef struct Vector
+{
+    int dimensions;
+    double *vals;
+} vector_t;
+
+vector_t * createVector(int dimensions);
+
+void vectorAdd(vector_t *u, vector_t *v);
+
+void vectorSub(vector_t *u, vector_t *v);
+
+void vectorScalarMul(vector_t *u, double scalar);
+
+double dotProduct(vector_t *u, vector_t *v);
+
+void normalizeVector(vector_t *u);
+
+void vectorFree(vector_t *u);
+
+#endif
