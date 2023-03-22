dfe32fc413acf3144277445695b52a0ba1bd7aaf
Ats <isfendiyar510@gmail.com>
2023-03-22 22:05:54 +0300

    Added vectorCopy

--- src/math/vector.c
+++ src/math/vector.c
@@ -1,10 +1,6 @@
 #include <stdlib.h>
 #include "vector.h"
 
-/*
-TODO: cross product
-*/
-
 vector_t * createVector(int dimensions)
 {
     vector_t *u = (vector_t *) malloc(sizeof(vector_t));
@@ -61,6 +57,14 @@ void normalizeVector(vector_t *u)
 
     for (i = 0; i<u->dimensions; i++)
         u->vals[i]/=max;
+}
+
+vector_t * vectorCopy(vector_t *u)
+{
+    vector_t *v = createVector(u->dimensions);
+    for (int i = 0; i<u->dimensions; i++)
+        v->vals[i] = u->vals[i];
+    return v;
 }
 
 // The program will die if you try to reference u later
